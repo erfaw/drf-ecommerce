645031aa8118662e825435c8ff1960951bd7fd4a
erfaw <erfawn.h@gmail.com>
2026-04-11 23:10:55 +0430

    feat: declare meta class for Category
    -MPTTMeta to order by name (idk what the fuck is about)

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
 
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
     def __str__(self):
         return self.name
 
