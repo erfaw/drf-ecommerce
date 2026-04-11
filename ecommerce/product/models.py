b3679abdcbbf3e9fea0625faa9810963b5217ad7
erfaw <erfawn.h@gmail.com>
2026-04-11 22:22:29 +0430

    feat: declare Brand table model
    -also declare ForeignKey for Product

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
-    brand = None # Gonna be rel to Brand model
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
