7d1b6e78b2d61624648d1822187c3b998ab18aa0
erfaw <erfawn.h@gmail.com>
2026-04-11 22:37:54 +0430

    feat: config __str__() for all models

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -6,9 +6,15 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -16,3 +22,6 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
