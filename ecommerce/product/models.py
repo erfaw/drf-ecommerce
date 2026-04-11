db2dcb8c523ba974b494289761fcde01edfa8cea
erfaw <erfawn.h@gmail.com>
2026-04-11 22:36:51 +0430

    feat: config TreeForeignKey for Category
    -using django-mptt

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from mptt.models import TreeForeignKey
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    parent = None  # must be hierarchial to itself
+    parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
 
 
 class Brand(models.Model):
