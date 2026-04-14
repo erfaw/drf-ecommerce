9a73d96d2437a03d845b46e005b2dbf21dc4291a
erfaw <erfawn.h@gmail.com>
2026-04-15 01:41:19 +0430

    feat: config url route for 'api/shema/' and 'api/schema/docs/'
    -using drf_spectacular and Swagger UI built-ins

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from ecommerce.product.views import CategoryViewSet
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 router = DefaultRouter()
@@ -10,4 +11,6 @@ router.register(r"category", CategoryViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
