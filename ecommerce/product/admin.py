594f8233120043a9e7385b4eb0339ad6bce4a7e5
erfaw <erfawn.h@gmail.com>
2026-04-23 01:29:47 +0430

    feat: config product app models with admin area

--- ecommerce/product/admin.py
+++ ecommerce/product/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Category, Brand, Product
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "parent")
+    list_filter = ("name",)
+    list_display_links = (
+        "id",
+        "name",
+    )
+    list_per_page = 25
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+    list_filter = ("name",)
+    list_display_links = (
+        "id",
+        "name",
+    )
+    list_per_page = 25
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+    list_filter = ("name",)
+    list_display_links = (
+        "id",
+        "name",
+    )
+    list_per_page = 25
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Product, ProductAdmin)
