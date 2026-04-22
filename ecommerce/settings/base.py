1722e13fc00d4845d992fa04ee84ba0fc58ddef5
erfaw <erfawn.h@gmail.com>
2026-04-22 12:00:10 +0430

    feat: config 'static' settings

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -71,6 +71,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'ecommerce' / 'static',
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
