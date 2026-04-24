b06a2d8b3e923eb17cbdd6b83fa1c75896f42781
erfaw <erfawn.h@gmail.com>
2026-04-24 10:39:27 +0430

    feat: declare BrandFactory
    -copy of CategoryFactory with brand naming

--- ecommerce/tests/factories.py
+++ ecommerce/tests/factories.py
@@ -1,5 +1,5 @@
 import factory
-from ecommerce.product.models import Category
+from ecommerce.product.models import Brand, Category
 
 
 class CategoryFactory(factory.django.DjangoModelFactory):
@@ -8,3 +8,11 @@ class CategoryFactory(factory.django.DjangoModelFactory):
 
     # Name of actual Category table model fields
     name = "test_category"
+
+
+class BrandFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Brand
+
+    # Name of actual Category table model fields
+    name = "test_brand"
