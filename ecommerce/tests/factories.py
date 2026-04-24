8d98c46ed2cc394256247ed2b60d3f1f27c6cbbb
erfaw <erfawn.h@gmail.com>
2026-04-24 13:08:40 +0430

    feat: add factory.Sequence to fields
    -in order to capable to test in any amount

--- ecommerce/tests/factories.py
+++ ecommerce/tests/factories.py
@@ -6,22 +6,22 @@ class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Category
 
-    name = "test_category"
+    name =  factory.Sequence(lambda n: f"category-{n}")
 
 
 class BrandFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Brand
 
-    name = "test_brand"
+    name = factory.Sequence(lambda n: f"brand-{n}")
 
 
 class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Product
 
-    name = "test_product"
-    description = "test_description"
+    name = factory.Sequence(lambda n: f"product-{n}")
+    description = factory.Sequence(lambda n: f"test_description-{n}")
     is_digital = True
     brand = factory.SubFactory(BrandFactory)
     category = factory.SubFactory(CategoryFactory)
