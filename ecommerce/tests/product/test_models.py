debd28b357250192a0f7710f001eff1b79113b70
erfaw <erfawn.h@gmail.com>
2026-04-24 09:00:51 +0430

    feat: basic structure of test for product.models
    -Category, Brand, Product
    -also make __init__ in tests/product to make it accessible as package

---
+++ ecommerce/tests/product/test_models.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from product.models import Category, Brand, Product
+
+
+class TestCategoryModel:
+    def test_str_method():
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+
+class TestBrandModel:
+    def test_str_method():
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+
+class TestProductModel:
+    def test_str_method():
+        # Arrange
+        # Act
+        # Assert
+        pass
