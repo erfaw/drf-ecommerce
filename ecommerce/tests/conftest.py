7f3f4b0155e2a8d2ca62655f69a47ad5dc9d43a8
erfaw <erfawn.h@gmail.com>
2026-04-24 12:26:35 +0430

    feat: make api_client() pytest fixture

--- ecommerce/tests/conftest.py
+++ ecommerce/tests/conftest.py
@@ -1,6 +1,13 @@
 from .factories import CategoryFactory, BrandFactory, ProductFactory
 from pytest_factoryboy import register
+from rest_framework.test import APIClient
+import pytest
 
 register(CategoryFactory)
 register(BrandFactory)
 register(ProductFactory)
+
+
+@pytest.fixture
+def api_client():
+    return APIClient
