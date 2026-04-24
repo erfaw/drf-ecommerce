5c2c427786c7343cdd70f06abef9686c45f1e3ff
erfaw <erfawn.h@gmail.com>
2026-04-24 12:40:29 +0430

    feat: make numbers of test obj and check response len
    -with json.loads and checking response.content
    -use category_factory.create_batch(4)

--- ecommerce/tests/product/test_endpoints.py
+++ ecommerce/tests/product/test_endpoints.py
@@ -1,15 +1,19 @@
 # E2E test for each endpoint
 import pytest
+import json
 
 pytestmark = pytest.mark.django_db
+
 
 class TestCategoryEndpoints:
     endpoint = r"/api/category/"
 
     def test_category_get(self, category_factory, api_client):
         # Arrange
-        category_factory()
+        category_factory.create_batch(4)
         # Act
         response = api_client().get(self.endpoint)
         # Assert
-        assert response.status_code == 200
+        assert response.status_code == 200 # response checking
+        assert len(json.loads(response.content)) == 4 # number checking
+        print(json.loads(response.content))
