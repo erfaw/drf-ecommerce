1fb18d4e0b6da53fee3a9cbb5734f346e9a091a3
erfaw <erfawn.h@gmail.com>
2026-04-25 10:55:34 +0430

    feat: test_product_line_get() for TestProductLineEndpoints()
    -just like previous endpoints

--- ecommerce/tests/product/test_endpoints.py
+++ ecommerce/tests/product/test_endpoints.py
@@ -45,3 +45,19 @@ class TestProductEndpoints:
         assert response.status_code == 200 # response checking
         assert len(json.loads(response.content)) == 4 # number checking
         print(json.loads(response.content))
+
+
+class TestProductLineEndpoints:
+    endpoint = r"/api/product-line/"
+    
+    def test_product_line_get(self, product_line_factory, api_client):
+        # Arrange
+        len_num = 5
+        product_line_factory.create_batch(len_num)
+        # Act
+        response = api_client().get(self.endpoint)
+        loaded_response = json.loads(response.content)
+        # Assert
+        assert response.status_code == 200
+        assert len(loaded_response) == len_num
+        print(loaded_response)
