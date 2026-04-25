2a81a854d92958431d7f62c0d6832960dce5d654
erfaw <erfawn.h@gmail.com>
2026-04-25 10:13:26 +0430

    feat: config ProductLine table model for admin area

--- ecommerce/product/admin.py
+++ ecommerce/product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Brand, Product
+from .models import Category, Brand, Product, ProductLine
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -38,6 +38,25 @@ class ProductAdmin(admin.ModelAdmin):
     list_per_page = 25
 
 
+class ProductLineAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "price",
+        "sku",
+        "stock_qty",
+        "product",
+        "is_active",
+    )
+    list_filter = ("product","is_active",)
+    list_display_links = (
+        "id",
+        "name",
+    )
+    list_per_page = 25
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductLine, ProductLineAdmin)
