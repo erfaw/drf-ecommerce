02ded14227ecb96bf0e917f04d20e2b90aab0716
erfaw <erfawn.h@gmail.com>
2026-04-25 10:00:27 +0430

    feat: register ProductLineViewSet() with router

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from ecommerce.product.views import CategoryViewSet, BrandViewSet, ProductViewSet
+from ecommerce.product.views import CategoryViewSet, BrandViewSet, ProductViewSet, ProductLineViewSet
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
@@ -9,6 +9,8 @@ router = DefaultRouter()
 router.register(r"category", CategoryViewSet)
 router.register(r"brand", BrandViewSet)
 router.register(r"product", ProductViewSet)
+router.register(r"product-line", ProductLineViewSet)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
