3ab25314d44dfc7d980e4f7251fa67c6d47478e4
erfaw <erfawn.h@gmail.com>
2026-04-25 09:14:23 +0430

    feat: declare ProductLine table model
    -with additional 'is_active' and 'name' field from Design

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -7,7 +7,7 @@ class Category(MPTTModel):
     parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
 
     class MPTTMeta:
-        order_insertion_by = ['name']
+        order_insertion_by = ["name"]
 
     def __str__(self):
         return self.name
@@ -26,6 +26,18 @@ class Product(models.Model):
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = TreeForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductLine(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    sku = models.CharField() 
+    stock_qty = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
