b1f6685e21c2da83af640fdebea3cb2047e4612f
erfaw <erfawn.h@gmail.com>
2026-04-29 10:26:12 +0430

    feat: get and assign 'unique_for_field' in OrderField.__init__()

--- ecommerce/product/fields.py
+++ ecommerce/product/fields.py
@@ -3,5 +3,6 @@ from django.db import models
 class OrderField(models.PositiveIntegerField):
     description = "Ordering number field on a Unique field."
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, unique_for_field=None, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
+        self.unique_for_field = unique_for_field
