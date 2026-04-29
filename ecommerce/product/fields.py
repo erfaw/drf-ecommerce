b25ebf82b515e65cf86f538476de80ed45af53f1
erfaw <erfawn.h@gmail.com>
2026-04-29 11:53:56 +0430

    feat: override *Field.check()
    -return parents and OrderField checks in OrderField.check()

--- ecommerce/product/fields.py
+++ ecommerce/product/fields.py
@@ -6,3 +6,12 @@ class OrderField(models.PositiveIntegerField):
     def __init__(self, unique_for_field=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.unique_for_field = unique_for_field
+
+    def check(self, **kwargs) -> list:
+        return [
+            *super().check(**kwargs),
+            *self._check_for_field(**kwargs),
+        ]
+    
+    def _check_for_field(self):
+        pass
