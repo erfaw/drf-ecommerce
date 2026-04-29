a3c6aa3b6d8718b97da8f8a3ada2e9db249de8d3
erfaw <erfawn.h@gmail.com>
2026-04-29 10:28:49 +0430

    fix: remove self in OrderField super().__init__()
    -is not necessary to send it, its automatic :)

--- ecommerce/product/fields.py
+++ ecommerce/product/fields.py
@@ -4,5 +4,5 @@ class OrderField(models.PositiveIntegerField):
     description = "Ordering number field on a Unique field."
 
     def __init__(self, unique_for_field=None, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.unique_for_field = unique_for_field
