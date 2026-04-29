eb529db133e4bc72c163f0bb3e4d1b4f5b2af584
erfaw <erfawn.h@gmail.com>
2026-04-29 10:24:22 +0430

    feat: call parent __init__() with super() and args and kwargs

--- ecommerce/product/fields.py
+++ ecommerce/product/fields.py
@@ -2,4 +2,6 @@ from django.db import models
 
 class OrderField(models.PositiveIntegerField):
     description = "Ordering number field on a Unique field."
-    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
