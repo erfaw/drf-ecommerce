4827ada84a2f8f498888bf3bcb3fb5503b4fb4c1
erfaw <erfawn.h@gmail.com>
2026-05-04 17:24:07 +0430

    feat: make ProductImageAdmin() and register it with admin.site

--- ecommerce/product/admin.py
+++ ecommerce/product/admin.py
@@ -83,7 +83,22 @@ class ProductLineAdmin(admin.ModelAdmin):
     list_per_page = 25
     inlines = [ProductImageInline]
 
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "url",
+        "alternative_text",
+        "order",
+    )
+    list_display_links = (
+        "id",
+        "name",
+        "url"
+    )
+    list_per_page = 25
 
+admin.site.register(ProductImage, ProductImageAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(Product, ProductAdmin)
